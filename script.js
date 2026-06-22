af0e246ede3c77e634b910d91188020d6988fa26
jaycobkarsemeyer-netizen <jaycobkarsemeyer@gmail.com>
2026-06-21 20:44:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,55 @@
 const svg = document.getElementById("canvas");
 
 function addRectangle() {
-    const rect = document.createElementNS(
-        "http://www.w3.org/2000/svg",
-        "rect"
-    );
+
+    const width =
+        Number(document.getElementById("rectWidth").value);
+
+    const height =
+        Number(document.getElementById("rectHeight").value);
+
+    const radius =
+        Number(document.getElementById("rectRadius").value);
+
+    const rect =
+        document.createElementNS(
+            "http://www.w3.org/2000/svg",
+            "rect"
+        );
 
     rect.setAttribute("x", 100);
     rect.setAttribute("y", 100);
-    rect.setAttribute("width", 200);
-    rect.setAttribute("height", 100);
+    rect.setAttribute("width", width);
+    rect.setAttribute("height", height);
+
+    rect.setAttribute("rx", radius);
+    rect.setAttribute("ry", radius);
+
     rect.setAttribute("fill", "none");
     rect.setAttribute("stroke", "black");
+    rect.setAttribute("stroke-width", "2");
 
     svg.appendChild(rect);
 }
 
 function addCircle() {
-    const circle = document.createElementNS(
-        "http://www.w3.org/2000/svg",
-        "circle"
-    );
 
-    circle.setAttribute("cx", 300);
-    circle.setAttribute("cy", 300);
-    circle.setAttribute("r", 50);
+    const diameter =
+        Number(document.getElementById("circleDiameter").value);
+
+    const circle =
+        document.createElementNS(
+            "http://www.w3.org/2000/svg",
+            "circle"
+        );
+
+    circle.setAttribute("cx", 400);
+    circle.setAttribute("cy", 250);
+    circle.setAttribute("r", diameter / 2);
+
     circle.setAttribute("fill", "none");
     circle.setAttribute("stroke", "blue");
+    circle.setAttribute("stroke-width", "2");
 
     svg.appendChild(circle);
 }
